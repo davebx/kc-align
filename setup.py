4784815b5a911313ce9703fb32dde7bec0d74f6c
Dave B <dave@bx.psu.edu>
2020-03-17 09:15:25 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kcalign",
-    version="0.5.3",
+    version="0.5.4",
     author="Nick Keener",
     author_email="nickeener@gmail.com",
     description="Kalgin-based codon-aware aligner for multiple sequences",
