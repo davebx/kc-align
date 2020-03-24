47f2751751c238c8a38966223e1573a42a6c6f02
Dave B <dave@bx.psu.edu>
2020-03-24 13:09:30 -0400

    Revert version update that did not include code changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kcalign",
-    version="0.5.7",
+    version="0.5.6",
     author="Nick Keener",
     author_email="nickeener@gmail.com",
     description="Kalgin-based codon-aware aligner for multiple sequences",
